ad42d2423cdc32f4d0efd17b502cfc4da4f7e77e
front-end-bull <2531085993@qq.com>
2017-11-21 17:30:41 +0800

    over

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const HOST = 'http://192.168.10.93:8080'
+const isString = function(obj){
+	  return Object.prototype.toString.call(obj) === "[object String]"; 
+}
+const str = JSON.stringify
+const l = console.log
