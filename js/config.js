5dff8aa3b824d7fcb923da630abf092ae92ec19a
front-end-bull <2531085993@qq.com>
2017-11-22 14:41:43 +0800

    over

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // const HOST = 'http://192.168.10.93:8080'
-const HOST = 'http://192.168.10.250:8000'
+const HOST = 'http://192.168.10.250:8080'
 // const HOST = 'http://52.77.187.2:4002'
 
 const isString = function(obj){
@@ -19,3 +19,22 @@ function GetQueryString(name) {
     r = null;  
     return context == null || context == "" || context == "undefined" ? "" : context;  
 }
+
+
+
+const utils = {
+	formatDate:function(now){
+	    var year = now.getYear() + 1900
+	    var month = now.getMonth() + 1
+	    var date = now.getDate()
+	    var hour = now.getHours()
+	    var minute = now.getMinutes()
+	    var second = now.getSeconds()
+	    if(month<10) month = '0'+month
+	    if(date<10) date = '0'+date
+
+	    return year + "-" + month + "-" + date + " " + hour + ":" + minute;
+	    // return year + "-" + month + "-" + date;
+	}
+}
+
---
+++ js/untitled.js
@@ -0,0 +1,43 @@
+if(new_lang == 'zh_HANS'){  
+    tmp_lang = 'zh_HANS';  
+    $('body').addClass('zh_HANS');  
+}
+if(new_lang == 'zh_HANT'){  
+    tmp_lang = 'zh_HANT';  
+    $('body').addClass('zh_HANT');  
+}
+if(new_lang == 'en'){  
+    tmp_lang = 'en';  
+    $('body').addClass('en');  
+} 
+if(new_lang == 'ml'){  
+    tmp_lang = 'ml';  
+    $('body').addClass('ml');  
+}  
+if(new_lang == 'in'){  
+    //印度尼西亚
+    tmp_lang = 'in';  
+    $('body').addClass('in');  
+}  
+if(new_lang == 'ar'){
+    tmp_lang = 'ar';  
+    $('body').addClass('ar'); 
+}
+if(new_lang == 'th'){
+    //泰国
+    tmp_lang = 'th';  
+    $('body').addClass('th'); 
+}
+if(new_lang == 'es'){
+    //西班牙
+    tmp_lang = 'es';  
+    $('body').addClass('es'); 
+}
+if(new_lang == 'ar'){
+    tmp_lang = 'ar';  
+    $('body').addClass('ar'); 
+}
+if(new_lang == 'ar'){
+    tmp_lang = 'ar';  
+    $('body').addClass('ar'); 
+}
