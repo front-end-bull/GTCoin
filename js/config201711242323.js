d3b93b02fdb5928e51e501f16b3c22c08dd299f8
front-end-bull <2531085993@qq.com>
2017-11-24 23:26:18 +0800

    over

--- js/config201711242323.js
+++ js/config201711242323.js
@@ -91,6 +91,12 @@ const utils = {
                 s += '0';    
             }    
             return s;    
+        },
+        comdify:function(n){
+            n += ''
+        　　 var re=/\d{1,3}(?=(\d{3})+$)/g;
+        　　 var n1=n.replace(/^(\d+)((\.\d+)?)$/,function(s,s1,s2){return s1.replace(re,"$&,")+s2;});
+        　　 return n1;
         }
 }
 
